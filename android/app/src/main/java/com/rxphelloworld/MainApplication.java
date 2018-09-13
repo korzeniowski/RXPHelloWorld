71be3b03e545193d2f031f1d70dedda475ef8123
Szymon Korzeniowski <szymon@s33.io>
2018-09-13 12:08:49 +0200

    Upgrade react-native to 0.57

--- android/app/src/main/java/com/rxphelloworld/MainApplication.java
+++ android/app/src/main/java/com/rxphelloworld/MainApplication.java
@@ -3,12 +3,10 @@ package com.rxphelloworld;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.horcrux.svg.SvgPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,10 +22,13 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactVideoPackage(),
-            new SvgPackage()
+          new MainReactPackage()
       );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
     }
   };
 
