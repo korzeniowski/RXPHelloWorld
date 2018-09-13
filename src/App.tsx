b27fe94844431187cb6da96f07f762c8b11a6f65
Szymon Korzeniowski <szymon@s33.io>
2018-09-13 11:46:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as RX  from 'reactxp'
+
+class App extends RX.Component<RX.CommonProps, RX.Stateless> {
+
+    render() {
+        return (
+            <RX.Text>
+                Welcome to ReactXP
+            </RX.Text>
+        );
+    }
+
+}
+
+export default App;
